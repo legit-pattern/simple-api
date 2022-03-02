4a1c65ced54e39595f1bddb325145a5ce69e737e
Mathias Ögärd <mathias.ogard@hogia.se>
2022-03-02 16:11:38 +0100

    Initial commit

---
+++ Controllers/TravellerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using simple_api.Engine;
+
+namespace simple_api.Controllers
+{
+    [Route("/")]
+    public class TravellerController : ControllerBase
+    {
+        public BoardingCardsHelper _boardingCardsHelper { get; set; }
+
+        public TravellerController(BoardingCardsHelper boardingCardsHelper)
+        {
+            _boardingCardsHelper = boardingCardsHelper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> Post([FromBody] List<string> collection)
+        {
+            return _boardingCardsHelper.OrganizeBoardingCards(collection);
+        }
+    }
+}
---
+++ Engine/BoardingCardsHelper.cs
@@ -0,0 +1,63 @@
+﻿using simple_api.Extensions;
+
+namespace simple_api.Engine
+{
+    public class BoardingCardsHelper
+    {
+        public List<string> OrganizeBoardingCards(List<string> collection)
+        {
+            // Example data
+            // JSON: [ "GOT-ARN", "HEL-GOT", "CPH-HEL" ]
+
+            var result = new List<string>();
+
+            var resultRaw = new List<KeyValuePair<string, string>>();
+
+
+            var from = new List<string>();
+            var to = new List<string>();
+
+            var dict = new Dictionary<string, string>();
+
+            // Separate data
+            foreach (var item in collection)
+            {
+                var pair = item.Split("-");
+                dict.Add(pair[0], pair[1]);
+
+                from.Add(pair[0]);
+                to.Add(pair[1]);
+            }
+
+            // Add/Find starting point
+            foreach (var f in from)
+            {
+                if (!to.Contains(f))
+                {
+                    resultRaw.Add(dict.GetEntry(f));
+                    break;
+                }
+            }
+
+            // Add the rest
+            var currentBoardingPass = resultRaw[0];
+            for (int i = 0; i < dict.Count; i++)
+            {
+                // Check if the current boarding pass' departure has a destination (Ask for permission)
+                if (from.Contains(currentBoardingPass.Value))
+                {
+                    currentBoardingPass = dict.GetEntry(currentBoardingPass.Value);
+                    resultRaw.Add(currentBoardingPass);
+                }
+            }
+
+            // Clean up so we can present nicely :) 
+            foreach (var item in resultRaw)
+            {
+                result.Add($"{item.Key}-{item.Value}");
+            }
+
+            return result;
+        }
+    }
+}
